3108269647187fb86b2c637b02bd9d2fa21575b7
Jesus David <jdavid_1385@yahoo.com>
2017-04-17 22:48:39 +0200

    Update csharp.js

--- sample-webtasks/csharp.js
+++ sample-webtasks/csharp.js
@@ -1,9 +1,43 @@
 /* Using C# via Edge.js: http://tjanczuk.github.io/edge */
 
-module.exports = function (cb) {
-    require('edge').func(function () {/*
-        async (dynamic context) => {
-            return "Hello, world from C#!";
+var edge = require('edge');
+
+var payload = { anInteger: 1, aNumber: 3.1415, aString: 'fooaaa', aBoolean: true, aBuffer: new Buffer(10), anArray: [ 1, 'fooaaaaa' ], anObject: { a: 'foaaaao', b: 12 };
+
+var getPerson = edge.func(function () {/*
+    using System.Threading.Tasks;
+
+    public class Person
+    {
+        public int anInteger = 1;
+        public double aNumber = 3.1415;
+        public string aString = "foo";
+        public bool aBoolean = true;
+        public byte[] aBuffer = new byte[10];
+        public object[] anArray = new object[] { 1, "foo" };
+        public object anObject = new { a = "foo", b = 12 };
+    }
+
+    public class Startup
+    {
+        public async Task<object> Invoke(dynamic input)
+        {
+            Person person = new Person()
+            {
+              anInteger = (int)input.anInteger,
+              aNumber = (double)input.aNumber,
+              aString = (string)input.aString,
+              aBoolean = (bool)input.aBoolean,
+              aBuffer = (byte[])input.aBuffer,
+              anArray = (object[])input.anArray,
+              anObject = (dynamic)input.anObject,
+            };
+            return person;
         }
-    */})(null, cb);
+    }
+*/});
+
+module.exports = function (context, cb) {
+    // getPerson(payload, cb);
+    getPerson(context.data, cb);
 }
